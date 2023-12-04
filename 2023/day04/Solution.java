69e0c82e13f9b8e08f21ee5f83bb823da96bd8e5
wurstbroteater <fleri94@web.de>
2023-12-04 20:07:02 +0100

    simplified 2023/day04

--- 2023/day04/Solution.java
+++ 2023/day04/Solution.java
@@ -7,52 +7,59 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution {
-	private record Card(int id, Set<Integer> winningNumbers, Set<Integer> numbers) {
-		Set<Integer> getMatchedNumbers() {
-			final Set<Integer> matchedNumbers = new HashSet<>(Set.copyOf(numbers));
-			matchedNumbers.retainAll(winningNumbers);
-			return matchedNumbers;
-		}
+    private static final List<Card> puzzle = readFile("ex_1");
 
-		int getPoints() {
-			return getMatchedNumbers().size() <= 1 ? getMatchedNumbers().size() : 2 << (getMatchedNumbers().size() - 2);
-		}
+    private record Card(int id, int points, Set<Integer> winningNumbers) {
 
-		Set<Integer> getEarnedScratchcardIds() {
-			final Set<Integer> out = new HashSet<>();
-			out.add(id);
-			for (int i = 1; i <= getMatchedNumbers().size(); i++) {
-				out.add(this.id + i);
-			}
-			if (out.size() == 1) {
-				final int idToResolve = out.stream().toList().get(0);
-				return puzzle.get(idToResolve - 1).getMatchedNumbers();
-			}
-			return out;
-		}
-	}
-	static final List<Card> puzzle = readFile("ex_1");
+        Set<Integer> getEarnedScratchcardIds() {
+            final Set<Integer> out = new HashSet<>();
+            for (int i = 1; i <= winningNumbers.size(); i++) {
+                out.add(this.id + i);
+            }
+            return out;
+        }
 
-	public static void main(String[] args) {
-		puzzle.forEach(it -> System.out.println(it.getEarnedScratchcardIds()));
-		System.out.println("Solution part 1: " + puzzle.stream().map(Card::getPoints).reduce(0, Integer::sum));
-	}
+        List<Card> getEarnedScratchcards() {
+            final List<Card> out = new ArrayList<>();
+            for (int i = 0; i < winningNumbers.size(); i++) {
+                out.add(puzzle.get(this.id + i));
+            }
+            return out;
+        }
 
-	private static List<Card> readFile(final String name) {
-		List<Card> out = new ArrayList<>();
-		try (final Stream<String> fileStream = Files.lines(Path.of(name), StandardCharsets.UTF_8)) {
-			fileStream.forEach(l -> {
-				String[] intermediate = l.split(": ");
-				final int id = Integer.parseInt(intermediate[0].split("Card ")[1].trim());
-				intermediate = intermediate[1].split(" \\| ");
-				out.add(new Card(id, Arrays.stream(intermediate[0].replaceAll(" {2}", " ").trim().split(" ")).map(e -> Integer.parseInt(e.trim()))
-						.collect(Collectors.toSet()),
-						Arrays.stream(intermediate[1].replaceAll(" {2}", " ").trim().split(" ")).map(e -> Integer.parseInt(e.trim()))
-								.collect(Collectors.toSet())));
-			});
-		} catch (IOException ex) {
-			throw new IllegalStateException("Error while parsing schematic file: " + ex.getCause());
-		}
-		return out;
-	}
+        @Override
+        public boolean equals(Object c) {
+            return c instanceof Card && this.id == ((Card) c).id;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        final List<Card> copies = new ArrayList<>();
+        puzzle.forEach(it -> {
+            var l = it.getEarnedScratchcards();
+            System.out.println(l.stream().map(Card::id).toList());
+            copies.addAll(l);
+        });
+        System.out.println("Solution part 1: " + puzzle.stream().map(Card::points).reduce(0, Integer::sum));
+        System.out.println(copies.stream().map(Card::id).toList());
+
+    }
+
+    private static List<Card> readFile(final String name) {
+        try (final Stream<String> fileStream = Files.lines(Path.of(name), StandardCharsets.UTF_8)) {
+            return fileStream.map(l -> {
+                String[] intermediate = l.split(": ");
+                final int id = Integer.parseInt(intermediate[0].split("Card ")[1].trim());
+                intermediate = intermediate[1].split(" \\| ");
+                final Set<Integer> winning = Arrays.stream(intermediate[0].replaceAll(" {2}", " ").trim().split(" ")).map(e -> Integer.parseInt(e.trim()))
+                        .collect(Collectors.toSet());
+                winning.retainAll(Arrays.stream(intermediate[1].replaceAll(" {2}", " ").trim().split(" ")).map(e -> Integer.parseInt(e.trim()))
+                        .collect(Collectors.toSet()));
+                return new Card(id, winning.size() <= 1 ? winning.size() : 2 << winning.size() - 2, winning);
+            }).toList();
+        } catch (IOException ex) {
+            throw new IllegalStateException("Error while parsing schematic file: " + ex.getCause());
+        }
+    }
 }

3b4661be7b62eed45ed12ec7c27e3cf9a7f53207
Eric <Fleri94@web.de>
2023-12-04 14:42:33 +0100

    WIP 2023/04 part 2

--- 2023/day04/Solution.java
+++ 2023/day04/Solution.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution {
-	private record Card(int id, Set<Integer> numbers, Set<Integer> winningNumbers) {
+	private record Card(int id, Set<Integer> winningNumbers, Set<Integer> numbers) {
 		Set<Integer> getMatchedNumbers() {
 			final Set<Integer> matchedNumbers = new HashSet<>(Set.copyOf(numbers));
 			matchedNumbers.retainAll(winningNumbers);
@@ -17,27 +17,34 @@ public class Solution {
 		int getPoints() {
 			return getMatchedNumbers().size() <= 1 ? getMatchedNumbers().size() : 2 << (getMatchedNumbers().size() - 2);
 		}
+
+		Set<Integer> getEarnedScratchcardIds() {
+			final Set<Integer> out = new HashSet<>();
+			out.add(id);
+			for (int i = 1; i <= getMatchedNumbers().size(); i++) {
+				out.add(this.id + i);
+			}
+			if (out.size() == 1) {
+				final int idToResolve = out.stream().toList().get(0);
+				return puzzle.get(idToResolve - 1).getMatchedNumbers();
+			}
+			return out;
+		}
 	}
+	static final List<Card> puzzle = readFile("ex_1");
 
 	public static void main(String[] args) {
-		final List<Card> puzzle = readFile("puzzle");
-		// System.out.println(puzzle.get(0));
-		// System.out.println(puzzle.get(0).getMatchedNumbers());
-		// System.out.println(puzzle.get(0).getPoints());
+		puzzle.forEach(it -> System.out.println(it.getEarnedScratchcardIds()));
 		System.out.println("Solution part 1: " + puzzle.stream().map(Card::getPoints).reduce(0, Integer::sum));
 	}
 
 	private static List<Card> readFile(final String name) {
 		List<Card> out = new ArrayList<>();
 		try (final Stream<String> fileStream = Files.lines(Path.of(name), StandardCharsets.UTF_8)) {
-
 			fileStream.forEach(l -> {
 				String[] intermediate = l.split(": ");
 				final int id = Integer.parseInt(intermediate[0].split("Card ")[1].trim());
 				intermediate = intermediate[1].split(" \\| ");
-				// System.out.println( Arrays.stream(intermediate[0].replaceAll(" {2}", " ").trim().split(" ")).toList());
-				// System.out.println( Arrays.stream(intermediate[1].replaceAll(" {2}", " ").trim().split(" ")).toList());
-				// System.out.println(id);
 				out.add(new Card(id, Arrays.stream(intermediate[0].replaceAll(" {2}", " ").trim().split(" ")).map(e -> Integer.parseInt(e.trim()))
 						.collect(Collectors.toSet()),
 						Arrays.stream(intermediate[1].replaceAll(" {2}", " ").trim().split(" ")).map(e -> Integer.parseInt(e.trim()))

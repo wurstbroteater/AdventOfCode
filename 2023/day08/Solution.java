dfe2caf3bbd95efc4ca04e13d4223d33372d09d1
wurstbroteater <fleri94@web.de>
2024-01-01 14:27:53 +0100

    Update 2023/08 part 1

--- 2023/day08/Solution.java
+++ 2023/day08/Solution.java
@@ -9,34 +9,33 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 public class Solution {
+    private static final String START_LABEL = "A";
+    private static final String END_LABEL = "Z";
 
-    private static final String START_LABEL = "AAA";
-    private static final String END_LABEL = "ZZZ";
-
-
-    record Network(String directions, Node startNode, Map<String, Node> nodes) {
+    record Network(String directions, List<Node> startNode, Map<String, Node> nodes) {
     }
 
     public static void main(String[] args) {
         final Network net = parseFile("puzzle");
-        System.out.println(net);
+        System.out.println("Solution part 1: Steps to " + END_LABEL + " were " + calcStepsToEndLabel(net.startNode.getFirst(), net.directions()));
+    }
+
+    private static int calcStepsToEndLabel(Node current, final String directions) {
         int i = 0;
-        Node current = net.startNode();
-        long steps = 0;
+        int steps = 0;
         while (true) {
-            if (i >= net.directions().length()) {
+            if (i >= directions.length()) {
                 i = 0;
             }
-            if (current.getName().equals(END_LABEL)) {
+            if (current.getName().endsWith(END_LABEL)) {
                 break;
             }
-            String dir = net.directions().charAt(i) + "";
+            final String dir = directions.charAt(i) + "";
             current = current.getSibling(dir);
             steps++;
             i++;
         }
-
-        System.out.println("Solution part 1: Steps to " + END_LABEL + " were " + steps);
+        return steps;
     }
 
     private static Network parseFile(final String name) {
@@ -55,7 +54,7 @@ public class Solution {
                 current.setL(nodes.get(intermediate[0]));
                 current.setR(nodes.get(intermediate[1]));
             });
-            return new Network(directions, nodes.get(START_LABEL), nodes);
+            return new Network(directions, nodes.values().stream().filter(n -> n.getName().endsWith(START_LABEL)).toList(), nodes);
         } catch (IOException ex) {
             throw new IllegalStateException("Error while parsing schematic file: " + ex.getCause());
         }

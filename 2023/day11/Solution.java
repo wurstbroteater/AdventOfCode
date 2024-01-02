0e1faf932711fdaf387c7adc047d13b59fc7ad05
wurstbroteater <fleri94@web.de>
2024-01-02 14:35:52 +0100

    Add Solution 2023/11 part 2

--- 2023/day11/Solution.java
+++ 2023/day11/Solution.java
@@ -5,52 +5,68 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Stream;
 
+
+//USE JAVA 21 OR HIGHER
 public class Solution {
-    private record Galaxy(int id, int x, int y) {
-        int distance(Galaxy galaxy) {
-            final int dx = Math.abs(galaxy.x() - this.x);
-            final int dy = Math.abs(galaxy.y() - this.y);
+    private record Galaxy(long id, long x, long y) {
+        long distance(Galaxy galaxy) {
+            final long dx = Math.abs(galaxy.x() - this.x);
+            final long dy = Math.abs(galaxy.y() - this.y);
             return dx + dy;
         }
     }
 
-    public static void main(String[] args) {
-        final List<Galaxy> galaxies = parseFile("puzzle");
-        final HashMap<String, Integer> shortestPaths = new HashMap<>();
-        galaxies.forEach(g -> {
-            for (Galaxy galaxy : galaxies) {
-                if (g.equals(galaxy)) {
-                    continue;
+    private record Universe(List<Galaxy> galaxies, List<Integer> emptyRows, List<Integer> emptyColumn) {
+        HashMap<String, Long> findShortestPaths() {
+            final HashMap<String, Long> shortestPaths = new HashMap<>();
+            galaxies.forEach(g -> {
+                for (Galaxy galaxy : galaxies) {
+                    if (g.equals(galaxy)) {
+                        continue;
+                    }
+                    final long distance = g.distance(galaxy);
+                    final String pathName = STR."\{Math.max(g.id(), galaxy.id())}<>\{Math.min(g.id(), galaxy.id())}";
+                    final Long currentBest = shortestPaths.getOrDefault(pathName, null);
+                    if (currentBest == null || currentBest > distance) {
+                        shortestPaths.put(pathName, distance);
+                    }
                 }
-                final int distance = g.distance(galaxy);
-                final String pathName = Math.max(g.id(), galaxy.id()) + "<>" + Math.min(g.id(), galaxy.id());
-                final Integer currentBest = shortestPaths.getOrDefault(pathName, null);
-                if (currentBest == null || currentBest > distance) {
-                    shortestPaths.put(pathName, distance);
-                }
-            }
-        });
-        System.out.println("Solution part 1: " + shortestPaths.values().stream().reduce(0, Integer::sum));
-    }
-
-
-    private static List<Galaxy> parseFile(final String name) {
-        try (final Stream<String> fileStream = Files.lines(Path.of(name), StandardCharsets.UTF_8)) {
-            final List<List<String>> grid = new ArrayList<>(fileStream.map(l -> new ArrayList<>(Arrays.stream(l.trim().split("")).toList())).toList());
-            expandGrid(grid);
-            return findGalaxies(grid);
-        } catch (IOException ex) {
-            throw new IllegalStateException("Error while parsing schematic file: " + ex.getCause());
+            });
+            return shortestPaths;
         }
     }
 
-    private static List<Galaxy> findGalaxies(final List<List<String>> grid) {
+    public static void main(String[] args) {
+        final List<Integer> universeExpansionFactors = List.of(2, 1000000);
+        for (int i = 0; i < universeExpansionFactors.size(); i++) {
+            System.out.println(STR."Solution part \{i == 0 ? 1 : 2}: \{parseFile("puzzle", universeExpansionFactors.get(i)).findShortestPaths().values().stream().reduce(0L, Long::sum)}");
+        }
+    }
+
+
+    private static Universe parseFile(final String name, final int universeExpansionFactor) {
+        try (final Stream<String> fileStream = Files.lines(Path.of(name), StandardCharsets.UTF_8)) {
+            final List<List<String>> grid = new ArrayList<>(fileStream.map(l -> new ArrayList<>(Arrays.stream(l.trim().split("")).toList())).toList());
+            return new Universe(findGalaxies(grid, universeExpansionFactor), findEmptyRows(grid), findEmptyColumns(grid));
+        } catch (IOException ex) {
+            throw new IllegalStateException(STR."Error while parsing schematic file: \{ex.getCause()}");
+        }
+    }
+
+    private static List<Galaxy> findGalaxies(final List<List<String>> grid, final int universeExpansionFactor) {
+        // both lists are sorted in descending order (max is first)
+        final List<Integer> emptyColumns = findEmptyColumns(grid);
+        final List<Integer> emptyRows = findEmptyRows(grid);
         final List<Galaxy> out = new ArrayList<>();
         int id = 1;
         for (int y = 0; y < grid.size(); y++) {
             for (int x = 0; x < grid.get(y).size(); x++) {
                 if (grid.get(y).get(x).equals("#")) {
-                    out.add(new Galaxy(id, x, y));
+                    int finalX = x;
+                    long xOffset = (universeExpansionFactor - 1) * emptyColumns.stream().filter(r -> r < finalX).count();
+                    int finalY = y;
+                    long yOffset = (universeExpansionFactor - 1) * emptyRows.stream().filter(r -> r < finalY).count();
+                    out.add(new Galaxy(id, x + xOffset, y + yOffset));
                     id++;
                 }
             }
@@ -58,13 +74,8 @@ public class Solution {
         return out;
     }
 
-    private static void expandGrid(List<List<String>> grid) {
-        final List<Integer> emptyColumns = findEmptyColumn(grid);
-        findEmptyRow(grid).forEach(r -> grid.add(r + 1, createEmptyRow(grid.getFirst().size())));
-        emptyColumns.forEach(c -> grid.forEach(l -> l.add(c + 1, ".")));
-    }
 
-    private static List<Integer> findEmptyRow(final List<List<String>> grid) {
+    private static List<Integer> findEmptyRows(final List<List<String>> grid) {
         final List<Integer> emptyRows = new ArrayList<>();
         for (int i = grid.size() - 1; i >= 0; i--) {
             if (grid.get(i).stream().allMatch(e -> e.equals("."))) {
@@ -74,7 +85,7 @@ public class Solution {
         return emptyRows;
     }
 
-    private static List<Integer> findEmptyColumn(final List<List<String>> grid) {
+    private static List<Integer> findEmptyColumns(final List<List<String>> grid) {
         final List<Integer> emptyRows = new ArrayList<>();
         for (int x = grid.getFirst().size() - 1; x >= 0; x--) {
             boolean addIt = true;
@@ -91,12 +102,5 @@ public class Solution {
         return emptyRows;
     }
 
-    private static List<String> createEmptyRow(final int length) {
-        final List<String> out = new ArrayList<>();
-        for (int i = 0; i < length; i++) {
-            out.add(".");
-        }
-        return out;
-    }
 
 }

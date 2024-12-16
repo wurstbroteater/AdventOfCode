59c122213cadaf3b4c37efd02ee2ecbc739f684f
wurstbroteater <fleri94@web.de>
2024-12-16 14:53:27 +0100

    finalize solution 2024/04

--- 2024/day04/Solution.java
+++ 2024/day04/Solution.java
@@ -2,8 +2,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
@@ -19,6 +19,7 @@ public class Solution {
     }
 
     private enum Direction {
+        //DO NOT CHANGE THIS ORDER!
         North, East, South, West;
 
         Point next(final Point current, final List<String> grid) {
@@ -44,7 +45,6 @@ public class Solution {
                     final int nextY = current.y() + 1 < grid.size() ? current.y() + 1 : -1;
                     yield new Point(nextX, nextY);
                 }
-
             };
         }
     }
@@ -57,40 +57,70 @@ public class Solution {
             final List<String> grid = parseFile(input);
 
             System.out.println(prefix + "Solution part 1: " + findXMAS(grid));
+            System.out.println(prefix + "Solution part 2: " + findMAS(grid));
         }
     }
 
     static long findXMAS(final List<String> grid) {
-        final char start = '1';
         long occurrences = 0;
         for (int y = 0; y < grid.size(); y++) {
             final String row = grid.get(y);
             //ex0 part 1-> 5
-            occurrences += countLineOccurrences(row, start);
+            occurrences += horizontalOccurrences(row);
             for (int x = 0; x < row.length(); x++) {
                 final char curr = row.charAt(x);
-                if (curr == start) {
-                    occurrences += findVertical(start, x, y, grid);
-                    occurrences += findDiagonal(start, x, y, grid);
+                if (curr == '1') {
+                    occurrences += findVertical(x, y, grid);
+                    occurrences += findDiagonal(x, y, grid);
                 }
             }
         }
         return occurrences;
     }
 
-    private static long findDiagonal(final char start, final int x, final int y, final List<String> grid) {
-        final String row = grid.get(y);
-        final char curr = row.charAt(x);
+    static long findMAS(final List<String> grid) {
+        final char aChar = '3';
         long occurrences = 0;
-        if (curr == start) {
-            final Point startPosition = new Point(x, y);
-            occurrences += findDiagonal(Direction.North, startPosition, grid);
-            occurrences += findDiagonal(Direction.East, startPosition, grid);
-            occurrences += findDiagonal(Direction.South, startPosition, grid);
-            occurrences += findDiagonal(Direction.West, startPosition, grid);
-        } else {
-            System.out.println("Invalid start for vertical search " + curr);
+        for (int y = 0; y < grid.size(); y++) {
+            final String row = grid.get(y);
+            for (int x = 0; x < row.length(); x++) {
+                final char curr = row.charAt(x);
+                if (curr == aChar) {
+                    final var sp = new Point(x, y);
+                    final var validNeighbours = Arrays.stream(Direction.values())
+                            .map(d -> d.next(sp, grid))
+                            .filter(Point::isValid)
+                            .toList();
+                    if (validNeighbours.size() == 4) {
+                        /*
+                         * the 4 valid neighbours are the diagonal neighbours of 'A' aka '3'. A valid combination of
+                         * such neighbours have to from the X with any shape "MAS". There are only 4 cases which fulfill
+                         * that criteria. The first is given the beginning of part two. By translating this example in
+                         * our int representation and start reading the numbers clockwise beginning at the top right, we
+                         * receive the String 4422. Doing so for the remaining 3 matrix transpositions yields in the
+                         * strings checked here. This works because the order of validNeighbours is assured by the order
+                         * Direction enum. Changing this the enum may break this implementation.
+                         */
+                        occurrences += Stream.of(validNeighbours.stream()
+                                        .map(p -> String.valueOf(p.getChar(grid)))
+                                        .reduce("", String::concat))
+                                .mapToLong(s -> "4422".equals(s) || "2442".equals(s) || "2244".equals(s) || "4224".equals(s) ? 1L : 0L)
+                                //there is always exactly 1 item, so this reduce is always 0 + 1 or 0 + 0
+                                .reduce(0L, Long::sum);
+                    }
+                }
+            }
         }
+        return occurrences;
+    }
+
+    private static long findDiagonal(final int x, final int y, final List<String> grid) {
+        long occurrences = 0;
+        final Point startPosition = new Point(x, y);
+        occurrences += findDiagonal(Direction.North, startPosition, grid);
+        occurrences += findDiagonal(Direction.East, startPosition, grid);
+        occurrences += findDiagonal(Direction.South, startPosition, grid);
+        occurrences += findDiagonal(Direction.West, startPosition, grid);
         return occurrences;
     }
 
@@ -107,17 +137,11 @@ public class Solution {
         return 0L;
     }
 
-    private static long findVertical(final char start, final int x, final int y, final List<String> grid) {
-        final String row = grid.get(y);
-        final char curr = row.charAt(x);
+    private static long findVertical(final int x, final int y, final List<String> grid) {
         long occurrences = 0;
-        if (curr == start) {
-            final Point startPosition = new Point(x, y);
-            occurrences += findVertical(1, startPosition, grid);
-            occurrences += findVertical(-1, startPosition, grid);
-        } else {
-            System.out.println("Invalid start for vertical search " + curr);
-        }
+        final Point startPosition = new Point(x, y);
+        occurrences += findVertical(1, startPosition, grid);
+        occurrences += findVertical(-1, startPosition, grid);
         return occurrences;
     }
 
@@ -141,8 +165,8 @@ public class Solution {
         return nextY;
     }
 
-    private static long countLineOccurrences(final String row, final char start) {
-        final List<String> target = generatePair(start);
+    private static long horizontalOccurrences(final String row) {
+        final List<String> target = List.of("1234", "4321");
         long count = 0;
         for (int i = 0; i <= row.length() - target.getFirst().length(); i++) {
             if (row.startsWith(target.getFirst(), i) || row.startsWith(target.getLast(), i)) {
@@ -150,13 +174,6 @@ public class Solution {
             }
         }
         return count;
-    }
-
-    private static List<String> generatePair(final char start) {
-        final int startIndex = Integer.parseInt(String.valueOf(start));
-        StringBuilder ordered = new StringBuilder();
-        IntStream.rangeClosed(startIndex, END).forEach(ordered::append);
-        return List.of(ordered.toString(), ordered.reverse().toString());
     }
 
     private static List<String> parseFile(final String name) {
